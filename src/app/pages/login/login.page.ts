89b24d3cc3a518cfb0d9a6dfa40e3cfc6fe5a899
cley <Jr.Bcley@gmail.com>
2024-03-22 11:58:55 -0300

    fix: refacting some page issues

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -9,8 +9,18 @@ export class LoginPage implements OnInit {
 
   constructor() { }
 
+  public getScreenWidth: any;
+  public getScreenHeight: any;
+
   ngOnInit() {
-    console.log();
+    this.getScreenWidth = window.innerWidth;
+    this.getScreenHeight = window.innerHeight;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+    this.getScreenHeight = window.innerHeight;
   }
 
 }
